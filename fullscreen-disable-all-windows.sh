6db9488fe7b3be4b25953aafd3994814bcd6731d
Jens Ungerer <github@jens-ungerer.de>
2024-04-12 22:40:25 +0200

    backup

---
+++ fullscreen-disable-all-windows.sh
@@ -0,0 +1,92 @@
+# # #!/bin/bash
+# # cf.: https://github.com/swaywm/sway/issues/4121
+# # Get available windows
+# #windows=$(swaymsg -t get_tree | jq -r '.nodes[1].nodes[].nodes[] | .. | (.id|tostring) + " " + .name?' | grep -e "[0-9]* ."  )
+
+# # # Get regular windows
+# regular_windows=$(swaymsg -t get_tree | jq -r '.nodes[1].nodes[].nodes[] | .. | (.id|tostring) + " " + .name?' | grep -e "[0-9]* ."  )
+
+# # # Get floating windows
+# floating_windows=$(swaymsg -t get_tree | jq '.nodes[1].nodes[].floating_nodes[] | (.id|tostring) + " " + .name?'| grep -e "[0-9]* ." | tr -d '"')
+
+# enter=$'\n'
+# if [[ $regular_windows && $floating_windows ]]; then
+#   all_windows="$regular_windows$enter$floating_windows"
+# elif [[ $regular_windows ]]; then
+#   all_windows=$regular_windows
+# else
+#   all_windows=$floating_windows
+# fi
+
+# echo $floating_windows
+# # for fn in $all_windows; do
+# #     abc=`$fn | awk '{print $1}'`
+# #     echo $abc
+# # done
+
+# # # Select window with rofi
+
+# # selected=$(echo "$windows" | awk '{print $1}')
+
+# # # Tell sway to disable full screen
+# # # swaymsg [con_id="$selected"] fullscreen disable
+# swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select(.pid and .visible) | {name} + .rect
+# #   | "\(.x),\(.y) \(.width)x\(.height) \(.name)"'
+
+# # https://gist.github.com/dshoreman/278091a17c08e30c46c7e7988b7c2f7d
+# abc=swaymsg -t get_tree | jq --argjson visible "$(swaymsg -t get_workspaces | jq -c '[.[] | select(.visible) | .id]')" '.. | (.nodes? // empty)[] | select(.type == "workspace" and .id == $visible[]) | .. | (.nodes? // empty)[] | select(.pid) | .rect | "\(.x),\(.y) \(.width)x\(.height)"'
+# echo $abc
+
+# | sed 's/}/n /'
+names=$(swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select(.pid and .visible) | {id}') 
+# code_val=$(jq --argjson '.name' <<< "$names")
+# code_val= names | jq --argjson name
+# echo $code_val
+# echo $names
+# keyValues=$(jq -n $names) | jq .name
+# echo jq '.name' <(echo "$names")
+
+# https://stackoverflow.com/questions/47105490/can-i-pass-a-string-variable-to-jq-not-the-file
+idsWhitespace=$(jq '.id' <<< $names)
+for s in $idsWhitespace
+do
+  swaymsg "[con_id="$((s))"] fullscreen disable"
+done
+# echo $idsWhitespace
+# theArray={`echo $names`}
+# echo $theArray
+# # IFS=' ' read -r -a array <<< "$idsWhitespace"
+# for element in "${array[@]}"
+# do
+#     echo "$element"
+# done
+# echo $names;
+# IFS='}' read -ra ADDR <<< "$names"
+# for i in "${ADDR[@]}"; do
+#   # process "$i"
+#   echo $i
+# done
+
+# while IFS='}' read -ra ADDR; do
+#   for i in "${ADDR[@]}"; do
+#     # process "$i"
+#     echo $i
+#   done
+# done <<< "$names"
+
+# echo $names
+
+# IFS= read -d '' DATA < $names  ## Imitates your DATA=$(wget ...). Just replace it.
+# while IFS=\" read -ra LINE; do
+#     case "${LINE[1]}" in
+#     name)
+#         # ERROR_MSG=${LINE[3]}
+#         # printf -v ERROR_MSG '%b' "${LINE[3]}"
+#         printf ${LINE[1])}
+#         ;;
+#     # success)
+#     #     # SUCCESS_MSG=${LINE[3]}
+#     #     printf -v SUCCESS_MSG '%b' "${LINE[3]}"
+#     #     ;;
+#     esac
+# done <<< "$DATA"
