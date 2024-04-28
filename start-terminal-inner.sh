23fc55325dd19e54ed1ec21b5f8a885f18ba1d3a
Jens Ungerer <github@jens-ungerer.de>
2024-04-28 11:03:21 +0200

    backup

---
+++ start-terminal-inner.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+swaymsg exec 'kitty --directory ~/';
---
+++ start-terminal.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. ~/.config/sway/fullscreen-disable-all-windows.sh;
+sleep 1s;
+. ~/.config/sway/start-terminal-inner.sh;
