17a9e29538de4727455f8014462d64d427c5f48a
Jens Ungerer <github@jens-ungerer.de>
2024-04-28 18:54:44 +0200

    running version of new 'start-terminal' approach via mod+Return

--- start-terminal.sh
+++ start-terminal.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+swaymsg "exec kitty ~/"
+sleep 1;
 . ~/.config/sway/fullscreen-disable-all-windows.sh;
-sleep 1s;
-. ~/.config/sway/start-terminal-inner.sh;
+sleep 1;
+swaymsg "[app_id="kitty"] fullscreen enable"
