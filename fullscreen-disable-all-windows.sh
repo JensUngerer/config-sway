184d07691a45c04fa16c47bb5b5f94b4bbad8a0b
Jens Ungerer <github@jens-ungerer.de>
2024-04-28 18:48:31 +0200

    extending to disable fullscreen by app_id

--- fullscreen-disable-all-windows.sh
+++ fullscreen-disable-all-windows.sh
@@ -2,9 +2,19 @@
 # # cf.: https://github.com/swaywm/sway/issues/4121
 # # https://gist.github.com/dshoreman/278091a17c08e30c46c7e7988b7c2f7d
 # https://stackoverflow.com/questions/47105490/can-i-pass-a-string-variable-to-jq-not-the-file
+
+# DEBUGGING:
+# swaymsg -t get_tree > ~/.config/sway/get_tree.json;
+
 readarray -t windows < <(swaymsg -t get_tree | jq -c '[.. | .floating_nodes?, .nodes? | .[]? | .nodes? | .[]? | select(.window!=null) | .window_properties | .instance ]')
 for win in $(echo "${windows}" | jq -r '.[]'); do
 # DEBUGGING:
-#  echo $win
+  # echo $win
   swaymsg "[instance="$win"] fullscreen disable"
+done
+readarray -t windows_by_app_id < <(swaymsg -t get_tree | jq -c '[.. | .floating_nodes?, .nodes? | .[]? | .nodes? | .[]? | .app_id | select(.!=null) ]')
+for win_by_app_id in $(echo "${windows_by_app_id}" | jq -r '.[]'); do
+# DEBUGGING:
+  # echo $win_by_app_id
+  swaymsg "[app_id="$win_by_app_id"] fullscreen disable"
 done
