c0bd829071ac479f934acbb49ee3615764f79610
Jens Ungerer <github@jens-ungerer.de>
2024-04-28 19:29:56 +0200

    adding try catch approach to fullscreen-disable script

--- fullscreen-disable-all-windows.sh
+++ fullscreen-disable-all-windows.sh
@@ -5,16 +5,23 @@
 
 # DEBUGGING:
 # swaymsg -t get_tree > ~/.config/sway/get_tree.json;
-
+{
 readarray -t windows < <(swaymsg -t get_tree | jq -c '[.. | .floating_nodes?, .nodes? | .[]? | .nodes? | .[]? | select(.window!=null) | .window_properties | .instance ]')
 for win in $(echo "${windows}" | jq -r '.[]'); do
 # DEBUGGING:
   # echo $win
   swaymsg "[instance="$win"] fullscreen disable"
 done
+} || {
+  echo "error when trying disable fullscreen by instance";
+}
+{
 readarray -t windows_by_app_id < <(swaymsg -t get_tree | jq -c '[.. | .floating_nodes?, .nodes? | .[]? | .nodes? | .[]? | .app_id | select(.!=null) ]')
 for win_by_app_id in $(echo "${windows_by_app_id}" | jq -r '.[]'); do
 # DEBUGGING:
   # echo $win_by_app_id
   swaymsg "[app_id="$win_by_app_id"] fullscreen disable"
 done
+} || {
+    echo "error when trying disable fullscreen by app_id";
+}
