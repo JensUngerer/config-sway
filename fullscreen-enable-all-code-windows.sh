060d56a7064e8f69313554276241bf5f503f3ed8
Jens Ungerer <github@jens-ungerer.de>
2024-05-01 15:31:30 +0200

    fixing bugs

--- fullscreen-enable-all-code-windows.sh
+++ fullscreen-enable-all-code-windows.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 . ~/.config/sway/fullscreen-disable-all-windows.sh;
 sleep 1;
-swaymessage=$(~/.config/sway/ide-instance.sh);
+swaymessage=$(~/.config/sway/ide-instance-id.sh);
 swaymessage+=" fullscreen enable";
+# DEBUGGING:
+# echo $swaymessage;
 swaymsg $swaymessage;
