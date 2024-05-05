d7893e990b92f0260dec95fe208b0b3b1c1f0f62
Jens Ungerer <github@jens-ungerer.de>
2024-05-05 09:52:56 +0200

    minor changes

--- start-first.sh
+++ start-first.sh
@@ -1,6 +1,4 @@
 # (. ~/.config/sway/play-battery-low-sound.sh &);
 source ~/.config/sway/sway-environment-variables.sh;
 sway --config ~/.config/sway/config-first-desktop;
-
 # echo 'sway has just been terminated';
-
