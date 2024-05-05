fb862ae6fc2da7e685ee22b98f6e7326b82bc25b
Jens Ungerer <github@jens-ungerer.de>
2024-05-05 12:20:01 +0200

    setting low batter on startup

--- start-first.sh
+++ start-first.sh
@@ -1,4 +1,6 @@
-# (. ~/.config/sway/play-battery-low-sound.sh &);
+systemctl --user start lowBattery.timer;
 source ~/.config/sway/sway-environment-variables.sh;
 sway --config ~/.config/sway/config-first-desktop;
-# echo 'sway has just been terminated';
+echo 'sway has just been terminated';
+# not necessary... as not loaded?
+# systemctl --user stop lowBattery.time;
